0b89e59d29d6ee937505911734cf8d8fd6115512
zhaoyiping <zhaoyiping0622@gmail.com>
2020-10-28 02:02:44 +0800

    modified interface names

--- src/backend/include/DbEngineInterface.h
+++ src/backend/include/DbEngineInterface.h
@@ -15,23 +15,19 @@ typedef struct Cursor Cursor;
  */
 Cursor* open(const char* dbTable, const char* indexName);
 int create(const char* dbTable, const char* indexName, CursorType indexType,
-           const int indexColumnCnt, const char** indexColumn);
+           const int indexColumnCnt, const char** indexColumns);
 int find(Cursor* cursor, const void* key);
-int findNext(Cursor* cursor);
 void* getKey(Cursor* cursor);
 void* getValue(Cursor* cursor);
-void* getRecordNumber(Cursor* cursor);
+// void* getRecordNumber(Cursor* cursor);
 int insert(Cursor* cursor, const void* key, const void* value);
 int erase(Cursor* cursor);
-int pre(Cursor* cursor);
 int next(Cursor* cursor);
 int reset(Cursor* cursor);
 
-/*
- * metadata相关操作
- */
 
-int createTable(const char* createTableSql);
-int reorganize(const char* dbTable);
-void* getInfo(const char* dbTable);
-int getNextRecordNumber(const char* dbTable);
+int createTable(const char* sql);
+int reorganize();
+void* getMetaData(const char* tableName);
+int getCookies();
+char** getTableColumns(const char* tableName);
