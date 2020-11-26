a4baafe3918c62268ff23dc9e207958e0b1ba33e
osmium18452 <1711042219@qq.com>
2020-11-26 14:31:20 +0800

    没看明白yyc的接口，摸了

--- src/backend/DBEngine/src/DBEngine.cpp
+++ src/backend/DBEngine/src/DBEngine.cpp
@@ -4,3 +4,59 @@
 #include "DBEngine.h"
 #include "BTreeInterface.h"
 #include "DbEngineInterface.h"
+
+/*
+ * realization of DbEngineInterface.h
+ */
+
+Cursor *open(const char *indexName, int flag) {
+	BPTree bpTree();
+	if (flag==CURSOR_BTREE){
+
+	} else {
+		return NULL;
+	}
+}
+
+int close(Cursor *cursor) {}
+
+int create(const char *dbTable, const char *indexName, CursorType indexType,
+           const int indexColumnCnt, const char **indexColumns) {}
+
+int find(Cursor *cursor, const void *key) {}
+
+void *getKey(Cursor *cursor) {}
+
+void *getValue(Cursor *cursor) {}
+
+// void* getRecordNumber(Cursor* cursor){}
+int insert(Cursor *cursor, const void *key, const void *value) {}
+
+int erase(Cursor *cursor) {}
+
+int next(Cursor *cursor) {}
+
+int reset(Cursor *cursor) {}
+
+
+int createTable(const char *sql) {}
+
+int reorganize() {}
+
+void *getMetaData(const char *tableName) {}
+
+int getCookies() {}
+
+char **getTableColumns(const char *tableName) {}
+
+/*
+ * 事务相关
+ */
+
+int transaction() {}
+
+int commit() {}
+
+int rollback() {}
+
+/*******************************************************************/
