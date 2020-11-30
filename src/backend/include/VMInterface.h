d63e29a6e27ad158af8bcf13d6477e98ecdf2663
zhaoyiping <zhaoyiping0622@gmail.com>
2020-11-30 19:26:53 +0800

    add `extern "C"` to VmInterface

--- src/backend/include/VMInterface.h
+++ src/backend/include/VMInterface.h
@@ -1,8 +1,9 @@
 #ifndef _VMINTERFACE_H
 #define _VMINTERFACE_H
 
+extern "C"{
 typedef int (*nmsql_callback)(void*,int,char**, char**);
-
 int exec(char* ir, nmsql_callback callback, void* args);
+}
 
 #endif
